b5509dc0f42e4da096995b638bae85419dcca504
Waqas Ali <waqas.ali.bsee@gmail.com>
2020-05-07 12:58:18 -0700

    catkin_ws create

---
+++ catkin_ws/build/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /home/robond/Desktop/Desktop_backup/projects/Project3/catkin_ws/src
+# Build directory: /home/robond/Desktop/Desktop_backup/projects/Project3/catkin_ws/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs(gtest)
